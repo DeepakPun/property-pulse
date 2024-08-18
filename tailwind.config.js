eb3e03e98f065a4f510c069aec85b8c1315bae1f
Deepak Pun <deepakpun@Deepaks-MacBook-Pro.local>
2024-08-18 13:52:01 -0400

    Google OAuth Setup complete. Users can now sign in and out using their gmail account

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,3 +17,22 @@ module.exports = {
 	plugins: [],
 }
 
+/*
+ Go to console.cloud.google.com
+ Create a project -  PropertyPulseNextJS
+ No organization - > create
+ click hamburger - api and oauths - oauth consent screen - external - apname=propertypulseapp, support email
+ contact info
+ save and continue
+ add or remove scopes - check => userinfo email and profile, update, save and continue
+ add users - add two gmail accounts save and continue
+ back to dashboard
+ click credentials -> create credentials -> oauth client id -> web application -> name it PropertyPulse
+ add uri -> http://localhost:3000 for auth js origin and http://localhost:3000/api/auth/callback/google for auth redirect uris
+ click create
+ copy client id and client secret
+ .env NEXTAUTH_URL=http://localhost:3000
+			NEXTAUTH_INTERNAL=http://localhost:3000
+			NEXTAUTH_SECRET=RW+FBIRiLSdbr67dRwp9QeMW6zk8OkDD/bNT2AlKk/M=
+*/
+
